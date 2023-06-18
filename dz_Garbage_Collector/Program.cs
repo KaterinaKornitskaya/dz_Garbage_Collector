0e63b58289ccb30984a46b10a824242f816c3bbf
Радченко Катерина <ketrinradchenko@gmail.com>
2023-06-18 20:09:30 +0300

    Add project files.

---
+++ dz_Garbage_Collector/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace dz_Garbage_Collector
+{
+    // Задание 1.
+    // Создайте класс Пьеса. Класс должен хранить: - название пьесы,
+    // - ФИО автора, - жанр, -год выпуска. Реализуйте методы и свойства.
+    // Добавьте в класс деструктор. Напишите код для для вызова
+    // деструктора.
+    // Задание 3.
+    // Добавьте у 1му заданию реализацтю интерфейса IDisposable.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Play p1 = new Play("Дети Ванюшина", "Сергей Найденов", "-", 1901);
+            Play p2 = new Play("На дне", "Максим Горький", "-", 1902);
+            Play p3 = new Play("Четырнадцатое июля", "Ромен Роллан", "-", 1902);
+            Play p4 = new Play("Вишневый сад", "Антон Чехов", "-", 1903);
+            Play p5 = new Play("Чудо святого Антония", "Морис Метерлинк", "-", 1903);
+            Play p6 = new Play("Мораль пани Дульской", "Габриеля Запольская", "-", 1906);
+            Play p7 = new Play("Соната призраков", "Август Стриндберг", "-", 1907);
+            Play p8 = new Play("Жизнь человека", "Леонид Андреев", "-", 1907);
+            Play p9 = new Play("Игра интересов", "Хасинто Бенавенте-и-Мартинес", "-", 1907);
+            
+            Play[] p_arr = { p1, p2, p3, p4, p5, p6, p7, p8, p9 };
+
+            foreach (Play item in p_arr)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            foreach (Play item in p_arr)
+            {
+                item.Dispose();
+            }
+           
+            Console.WriteLine($"Поколение объекта: {GC.GetGeneration(p_arr)}");          
+            Console.WriteLine($"Занято памяти (байт): {GC.GetTotalMemory(false)}");
+            
+            GC.Collect();
+
+            Console.WriteLine($"Поколение объекта: {GC.GetGeneration(p_arr)}");
+            Console.WriteLine($"Занято памяти (байт): {GC.GetTotalMemory(false)}");
+        }
+    }
+
+    class Play : IDisposable
+    {
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+
+        public Play(string title, string authorName, string genre, int year)
+        {
+            Title=title;
+            AuthorName=authorName;
+            Genre=genre;
+            Year=year;
+        }
+
+        public override string ToString()
+        {
+            return $"Play title\t {Title} \nAuthor name\t {AuthorName}" +
+                $"\nPlay genre\t {Genre} \nYear of edition\t {Year}\n";
+        }
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose.");
+            Title = null;
+            AuthorName = null;
+            Genre  =null;
+            Year = 0;
+        }
+        ~Play()
+        {
+            Console.WriteLine("Play - Деструктор вызван.");
+            Dispose();
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,118 @@
+﻿namespace task2
+{
+    class Shop : IDisposable
+    {
+        private string name;
+        private string address;
+        private string type_shope;
+
+        public Shop()
+        {
+            name = "-";
+            address = "-";
+            type_shope = "-";
+        }
+
+        public void Show_shop()
+        {
+            Console.WriteLine($"\t___Shop___" +
+                              $"\nName: {name}" +
+                              $"\nAddress: {address}" +
+                              $"\nType_shope: {type_shope}");
+        }
+
+        public void New_shop()
+        {
+            bool FLAG = true;
+
+            while (FLAG)
+            {
+                Console.WriteLine("Создание магазина");
+                Console.WriteLine("1. Создать магазин");
+                Console.WriteLine("2. Показать магазин");
+                Console.WriteLine("3. Выход");
+                Console.Write("Введите ваш выбор: ");
+                string choice = Console.ReadLine();
+                if (choice == "1")
+                {
+                    Console.Write("Введите название магазина: ");
+                    name = Console.ReadLine();
+                    Console.Write("Введите адрес магазина: ");
+                    address = Console.ReadLine();
+                    Console.WriteLine("Выбери тип магазина:" +
+                                      "\n1. Продовольственный" +
+                                      "\n2. Хозяйственный" +
+                                      "\n3. Одежда" +
+                                      "\n4. Обувь" +
+                                      "\n5. Выход");
+                    Console.Write("Введите ваш выбор: ");
+                    int choice_shop = Int32.Parse(Console.ReadLine());
+                    switch(choice_shop)
+                    {
+                        case 1:
+                            type_shope = "Продовольственный";
+                            Console.WriteLine($"\nТип выбран: {type_shope}");
+                            break;
+                        case 2:
+                            type_shope = "Хозяйственный";
+                            Console.WriteLine($"\nТип выбран: {type_shope}");
+                            break;
+                        case 3:
+                            type_shope = "Одежда";
+                            Console.WriteLine($"\nТип выбран: {type_shope}");
+                            break;
+                        case 4:
+                            type_shope = "Обувь";
+                            Console.WriteLine($"\nТип выбран: {type_shope}");
+                            break;
+                        case 5:
+                            Console.WriteLine("Спасибо за выбор!");
+                            Show_shop();
+                            break;
+                        default:
+                            Console.WriteLine("Ошибка выбора: введите корректно");
+                            break;
+
+                    }
+                }
+                else if (choice == "2")
+                {
+                    Show_shop();
+                }
+                else if (choice == "3")
+                {
+                    Console.WriteLine("Новый магазин был создан!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неправильный выбор.");
+                }
+            }
+        }
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose");
+            name = null;
+            address = null;
+            type_shope = null;
+        }
+
+        ~Shop()
+        {
+            Console.WriteLine("Деструктор");
+            Dispose();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop _shop = new Shop();
+            _shop.New_shop();
+            _shop.Dispose();
+            Console.Read();
+        }
+    }
+}
